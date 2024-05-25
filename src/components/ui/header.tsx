19342629b021aae5fda58e155c2af37d2b65c8c5
bysxx <0415white@gmail.com>
2024-05-25 15:34:58 +0900

    fix: fix image location

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { LogoSVG } from 'src/assets/CommonIcons';
+'use client';
+
 import Image from 'next/image';
 
 // 윤성님 수정코드
@@ -20,7 +20,7 @@ function Header() {
 //   return (
 //     <div className="w-full">
 //       <div className="flex items-center">
-//         <LogoSVG /> 
+//         <LogoSVG />
 //         <Image src="/logo.svg" alt="logo" width={22} height={22} />
 //         <div className="text-[22px] font-bold ml-1.5">SUB</div>
 //       </div>
