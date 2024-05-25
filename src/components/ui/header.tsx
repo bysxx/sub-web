bd2d579485668c2c346e31e0cf368135f1fc3d0d
bysxx <0415white@gmail.com>
2024-05-25 15:15:56 +0900

    feat: update header image

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,25 +1,23 @@
-"use client";
-import LogoSVG from "public/logo.svg";
-import Image from 'next/image';
-import Head from 'next/head';
+'use client';
 
+import Image from 'next/image';
 
 function Header() {
-    return (
-      <div className="w-full">
-        <div>
-          {/* <LogoSVG />  */}
-          <Image src={LogoSVG} alt="logo" width={22} />
-          <h1 className="text-[22px] font-bold">SUB</h1>
-        </div>
+  return (
+    <div className="w-full">
+      <div className="flex gap-1">
+        {/* <LogoSVG />  */}
+        <Image src="/logo.svg" alt="logo" width={22} height={22} />
+        <h1 className="text-[22px] font-bold">SUB</h1>
       </div>
-      // <div className="bg-gray-100 min-h-screen">
-      // <Head>
-      // <LogoSVG /> 
-      //   <title>SUB</title>
-      // </Head>
-      // </div>
-    );
+    </div>
+    // <div className="bg-gray-100 min-h-screen">
+    // <Head>
+    // <LogoSVG />
+    //   <title>SUB</title>
+    // </Head>
+    // </div>
+  );
 }
 
 export default Header;
