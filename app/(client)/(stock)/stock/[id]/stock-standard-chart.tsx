bb4b17a2446bd165ce640b0d65e7b73384e4d868
whaleflyingsky <sunbeanp@hanyang.ac.kr>
2024-06-05 21:57:32 +0900

    design: change tooltip color

--- app/(client)/(stock)/stock/[id]/stock-standard-chart.tsx
+++ app/(client)/(stock)/stock/[id]/stock-standard-chart.tsx
@@ -28,7 +28,7 @@ const fetchStockData = async (stockId: any): Promise<Product> => {
 const CustomTooltip = ({ active, payload, label }: CustomTooltipProps) => {
   if (active && payload && payload.length && label !== '0') {
     return (
-      <div className="rounded-md bg-white p-2 text-[12px] font-semibold text-primary-b200 opacity-80">
+      <div className="rounded-md bg-white p-2 text-[12px] font-semibold text-secondary-y100 opacity-80">
         <p>{`${payload[0].value}서브`}</p>
       </div>
     );
